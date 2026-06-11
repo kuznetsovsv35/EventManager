903b62b3f03e9de293ae1389755aaaf227870ba8
Кузнецов Сергей <kuznretsov.sv.35@yandex.ru>
2026-06-12 00:01:54 +0300

    Создание модели события.

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,6 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EventManager.Models;
 
 public class Event
 {
+    [Required]
+    public required Guid Id { get; set; } = Guid.NewGuid();
     
+    [Required]
+    public required string Title { get; set; } = string.Empty;
+    
+    public string? Description { get; set; }
+    
+    [Required]
+    public required DateTime StartAt { get; set; } = DateTime.Now;
+    
+    [Required]
+    public DateTime EndAt { get; set; } = DateTime.Now.AddMinutes(30);
 }
