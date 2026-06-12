846e03ee7a3ed7c373322baae00b3c13b7c3a341
Кузнецов Сергей <kuznretsov.sv.35@yandex.ru>
2026-06-12 04:19:30 +0300

    Поддержка атрибута валидации.

---
+++ Application/DTO/DataValidation.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EventManager.Application.DataTransfer;
+
+public class DataValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is EventInputData data)
+        {
+            if (string.IsNullOrWhiteSpace(data.Title))
+                return new ValidationResult($"Свойство \"{nameof(data.Title)}\" не может быть пустым.");
+
+            if (data.EndAt <= data.StartAt)
+                return new ValidationResult("Окончание не может быть раньше начала события.");
+            
+            return ValidationResult.Success;
+        }
+        return base.IsValid(value, validationContext);
+    } 
+}
--- Application/DTO/EventInputData.cs
+++ Application/DTO/EventInputData.cs
@@ -1,15 +1,20 @@
+using System.ComponentModel.DataAnnotations;
 using EventManager.Models;
 
 namespace EventManager.Application.DataTransfer;
 
+[DataValidation]
 public class EventInputData
 {
+    [Required(AllowEmptyStrings = false, ErrorMessage = $"Свойство \"{nameof(Title)}\" не может быть пустым.")]
     public string? Title { get; set; }
     
     public string? Description { get; set; }
     
+    [Required(ErrorMessage = $"Свойство \"{nameof(StartAt)}\" не может быть пустым.")]
     public DateTime StartAt { get; set; }
     
+    [Required(ErrorMessage = $"Свойство \"{nameof(EndAt)}\" не может быть пустым.")]
     public DateTime EndAt { get; set; }
 
     public EventInputData() {}
@@ -22,15 +27,23 @@ public class EventInputData
         EndAt = source.EndAt;
     }
 
-    internal Event ToEvent()
+    void Check()
     {
         if (string.IsNullOrWhiteSpace(Title))
             throw new ArgumentException(nameof(Title));
+        
+        if (EndAt <= StartAt)
+            throw new ArgumentException("Окончание не может быть раньше начала события.");
+    }
+
+    internal Event ToEvent()
+    {
+        Check();
 
         return new Event()
         {
             Id = Guid.NewGuid(),
-            Title = this.Title,
+            Title = this.Title!,
             Description = this.Description,
             StartAt = this.StartAt,
             EndAt = this.EndAt
@@ -39,10 +52,9 @@ public class EventInputData
 
     internal void Update(Event dest)
     {
-        if (string.IsNullOrWhiteSpace(Title))
-            throw new ArgumentException(nameof(Title));
+        Check();
 
-        dest.Title = Title;
+        dest.Title = Title!;
         dest.Description = Description;
         dest.StartAt = StartAt;
         dest.EndAt = EndAt;
