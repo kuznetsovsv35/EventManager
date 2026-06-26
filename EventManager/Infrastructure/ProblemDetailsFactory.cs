ba6791bdb4b8a17696fc2d3477525b7e3ab4334c
Kuznetsov Sergey <kuznetsov.sv.35@yandex.com>
2026-06-26 04:06:53 -0500

    Инфраструктура: добавлена фабрика ProblemDetails.

---
+++ EventManager/Infrastructure/ProblemDetailsFactory.cs
@@ -0,0 +1,60 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManager.Infrastructure;
+
+public class ProblemDetailsBuilder<T>(T problem) where T : ProblemDetails
+{
+    public T Problem { get; } = problem;
+
+    public ProblemDetailsBuilder<T> Build(Action<T> build)
+    {
+        build?.Invoke(Problem);
+        return this;
+    }
+
+    public ProblemDetailsBuilder<T> AddInstance(string instance)
+    {
+        Problem.Instance = instance;
+        return this;
+    }
+}
+
+public static class ProblemDetailsFactory
+{
+    public static ProblemDetailsBuilder<ProblemDetails> InternalServiceError(string? detail = null, string? title = null)
+        => new(new()
+        {
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
+            Title = title ?? "Внутренняя ошибка сервера",
+            Detail = detail ?? "Произошла непредвиденная ошибка. Попробуйте повторить запрос позже.",
+            Status = StatusCodes.Status500InternalServerError,
+        });
+
+    public static ProblemDetailsBuilder<ProblemDetails> VakidationProblem(ValidationResult result, string message, string? detail = null, string? title = null)
+    {
+        var details = new ValidationProblemDetails
+        {
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1",
+            Title = title ?? "Ошибка валидации запроса",
+            Detail = detail ?? "Один или несколько параметров содержат недопустимые значения.",
+            Status = StatusCodes.Status400BadRequest,
+        };
+
+        foreach (var member in result.MemberNames)
+        {
+            details.Errors[member] = [result.ErrorMessage ?? message];
+        }
+
+        return new(details);
+    }
+
+    public static ProblemDetailsBuilder<ProblemDetails> NotFound(string? detail = null, string? title = null)
+        => new(new()
+        {
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4",
+            Title = title ?? "Ресулс не найден.",
+            Detail = detail ?? "Ресурс не найден.",
+            Status = StatusCodes.Status404NotFound,
+        });
+}
