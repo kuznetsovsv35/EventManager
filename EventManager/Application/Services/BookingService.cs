5982703afe19313a1695d8e5d538cf884e88de7c
Кузнецов Сергей <kuznretsov.sv.35@yandex.ru>
2026-07-11 14:36:01 +0300

    Сервис бронирования: реализованы методы создания/получения данных брони.

--- EventManager/Application/Services/BookingService.cs
+++ EventManager/Application/Services/BookingService.cs
@@ -1,5 +1,7 @@
 using EventManager.Application.Interfaces;
+using EventManager.Infrastructure;
 using EventManager.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventManager.Application.Services;
 
@@ -7,11 +9,31 @@ public class BookingService(IAppDbContext dbContext) : IBookingService
 {
     public async Task<Booking> CreateBookingAsync(Guid eventId)
     {
-        throw new NotImplementedException();
+        if (await dbContext.Events.SingleOrDefaultAsync(x => x.Id == eventId) is Event @event)
+        {
+            var booking = new Booking()
+            {
+                Id = Guid.NewGuid(),
+                EventId = eventId,
+                CreatedAt = DateTime.Now,
+                Status = BookingStatus.Pending
+            };
+
+            await dbContext.AddBookingAsync(booking);
+
+            return booking;
+        }
+
+        throw new EventNotFoundException(nameof(eventId), eventId);
     }
 
     public async Task<Booking> GetBookingByIdAsync(Guid bookingId)
     {
-        throw new NotImplementedException();
+        if (await dbContext.Bookings.SingleOrDefaultAsync(x => x.Id == bookingId) is Booking booking)
+        {
+            return booking;    
+        }
+        
+        throw new BookingNotFoundException(nameof(bookingId), bookingId);
     }
 }
