ddf8e61aebc805c634ee6e63710b8f1053a419cd
Кузнецов Сергей <kuznretsov.sv.35@yandex.ru>
2026-07-14 00:32:32 +0300

    Регистрации асинхронной очереди.

--- EventManager/Application/DependencyInjection.cs
+++ EventManager/Application/DependencyInjection.cs
@@ -16,7 +16,7 @@ public static class DependencyInjection
         services.AddScoped<IPaginator<Event>, PaginateService<Event>>();
         services.AddScoped<IEventService, EventService>();
         services.AddScoped<IBookingService, BookingService>();
-        services.AddSingleton<IBookingQueue, BookingQueue>();
+        services.AddSingleton<IAsyncQueue<Booking>, AsyncQueue<Booking>>();
         services.AddHostedService<AppBackgroundService>();
         return services;
     }
