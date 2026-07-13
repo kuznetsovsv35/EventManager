04148d5bdbcd1918b7e6485d6c00a877d0595a24
Кузнецов Сергей <kuznretsov.sv.35@yandex.ru>
2026-07-14 00:34:22 +0300

    Фикс тестов, доп. тесты.

--- EventManager.Tests/BookingServiceFixture.cs
+++ EventManager.Tests/BookingServiceFixture.cs
@@ -1,5 +1,6 @@
 using EventManager.Application.Interfaces;
 using EventManager.Application.Services;
+using EventManager.Models;
 
 namespace EventManager.Tests;
 
@@ -8,5 +9,5 @@ public class BookingServiceFixture  : TestAppDbContext
     public IBookingService BookingService { get; }
 
     public BookingServiceFixture() : base()
-        => BookingService = new BookingService(this, new BookingQueue());
+        => BookingService = new BookingService(this, new AsyncQueue<Booking>());
 }
--- EventManager.Tests/BookingServiceTest.cs
+++ EventManager.Tests/BookingServiceTest.cs
@@ -1,3 +1,5 @@
+using EventManager.Application.DataTransfer;
+using EventManager.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventManager.Tests;
@@ -17,5 +19,55 @@ public class BookingServiceTest(BookingServiceFixture fixture) : TraitAttributes
     
         // Then
         Assert.Equal(eventId, bookingInfo.EventId);
+        Assert.Equal(BookingStatus.Pending, bookingInfo.Status);
+        Assert.Null(bookingInfo.ProcessedAt);
+    }
+
+    [Trait(Category, Category_Booking)]
+    [Theory]
+    [InlineData([10])]
+    [InlineData([20])]
+    [InlineData([50])]
+    public async Task CreateMultiBookingsForEvent_Success(int bookingCount)
+    {
+        // Given
+        using var cts = new CancellationTokenSource();
+        var eventCount = await fixture.Events.CountAsync(cts.Token);
+        var eventIndex = Random.Shared.Next(eventCount);
+        var eventId = (await fixture.Events.Skip(eventIndex).FirstAsync(cts.Token)).Id;
+    
+        // When
+        Task<BookingInfo>[] tasks = [.. Enumerable
+            .Range(0, bookingCount)
+            .Select(_ => fixture.BookingService.CreateBookingAsync(eventId, cts.Token))];
+        
+        var bookingIds = tasks.Select(t => t.Result.Id).ToHashSet();
+    
+        // Then
+        Assert.Equal(bookingCount, bookingIds.Count);
+    }
+
+    [Trait(Category, Category_Booking)]
+    [Fact]
+    public async Task GetBookingById_Success()
+    {
+        // Given
+        using var cts = new CancellationTokenSource();
+        var eventCount = await fixture.Events.CountAsync(cts.Token);
+        var eventIndex = Random.Shared.Next(eventCount);
+        var eventId = (await fixture.Events.Skip(eventIndex).FirstAsync(cts.Token)).Id;
+
+        // When
+        var bookingCreated = await fixture.BookingService.CreateBookingAsync(eventId, cts.Token);
+        var bookingFound = await fixture.BookingService.GetBookingByIdAsync(bookingCreated.Id, cts.Token);
+    
+        // Then
+        Assert.Equal(bookingCreated.Id, bookingFound.Id);
+        Assert.Equal(eventId, bookingCreated.EventId);
+        Assert.Equal(bookingCreated.EventId, bookingFound.EventId);
+        Assert.Equal(BookingStatus.Pending, bookingCreated.Status);
+        Assert.Equal(bookingCreated.Status, bookingFound.Status);
+        Assert.Null(bookingCreated.ProcessedAt);
+        Assert.Null(bookingFound.ProcessedAt);
     }
 }
