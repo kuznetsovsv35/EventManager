5399dce259155d10e8f4b18e6352aff86c5077c6
Кузнецов Сергей <kuznretsov.sv.35@yandex.ru>
2026-07-14 00:28:17 +0300

    BookingInfo: сравнения объектов.

--- EventManager/Application/DTO/BookingInfo.cs
+++ EventManager/Application/DTO/BookingInfo.cs
@@ -13,4 +13,22 @@ public class BookingInfo
     public required DateTime CreatedAt { get; init; }
 
     public DateTime? ProcessedAt { get; init; }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+            return true;
+
+        if (obj is BookingInfo info)
+            return CreatedAt == info.CreatedAt
+                && EventId == info.EventId
+                && Id == info.Id
+                && ProcessedAt ==info.ProcessedAt
+                && Status == info.Status;
+
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+        => Id.GetHashCode();
 }
