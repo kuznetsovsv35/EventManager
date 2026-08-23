7881467e0ecea344e8ac3c383f67033c9474a1ed
Кузнецов Сергей <kuznretsov.sv.35@yandex.ru>
2026-08-24 01:28:52 +0300

    Конфигурация схемы БД.

---
+++ EventManager/Data/Configuration/EventConfiguration.cs
@@ -0,0 +1,28 @@
+using EventManager.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventManager.Data.Configuration;
+
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder.ToTable("Events");
+
+        builder.Property(e => e.Id).IsRequired().ValueGeneratedNever();
+        builder.Property(e => e.Title).IsRequired().HasMaxLength(200);
+        builder.Property(e => e.StartAt).IsRequired();
+        builder.Property(e => e.EndAt).IsRequired();
+        builder.Property(e => e.TotalSeats).IsRequired().HasDefaultValue(0);
+        builder.Property(e => e.ReservedCount).IsRequired().HasDefaultValue(0);
+        builder.Ignore(e => e.AvailableSeats);
+
+        builder.HasKey(e => e.Id);
+        
+        builder.HasMany(e => e.Bookings)
+            .WithOne(b => b.Event)
+            .HasForeignKey(b => b.EventId)
+            .OnDelete(DeleteBehavior.Cascade);
+}
+}
