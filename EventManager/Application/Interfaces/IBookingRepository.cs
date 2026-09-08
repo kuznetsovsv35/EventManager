14fa649b7f784149264e9a0d23600095c56148ef
Kuznetsov Sergey <kuznetsov.sv.35@yandex.com>
2026-09-08 08:54:53 -0500

    Доработка репозитория броней

--- EventManager/Application/Interfaces/IBookingRepository.cs
+++ EventManager/Application/Interfaces/IBookingRepository.cs
@@ -2,11 +2,37 @@ using EventManager.Models;
 
 namespace EventManager.Application.Interfaces;
 
+/// <summary>
+/// Интерфейс репозитория бронирований.
+/// </summary>
 public interface IBookingRepository
 {
+    /// <summary>
+    /// Сохраняет созданную бронь.
+    /// </summary>
+    /// <param name="booking"></param>
+    /// <param name="cancellation"></param>
+    /// <returns></returns>
     Task AddBooking(Booking booking, CancellationToken cancellation);
-    
+    /// <summary>
+    /// Возвращает бронь по идентификатору.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cancellation"></param>
+    /// <returns></returns>
     Task<Booking?> GetBookingAsync(Guid id, CancellationToken cancellation);
-    
+    /// <summary>
+    /// Обновляет состояние брони.
+    /// </summary>
+    /// <param name="booking"></param>
+    /// <param name="cancellation"></param>
+    /// <returns></returns>
     Task UpdateBookingStatusAsync(Booking booking, CancellationToken cancellation);
+    /// <summary>
+    /// Возвращает все "ожидающие" брони (для пакетной обработки)
+    /// </summary>
+    /// <param name="chunkSize">Размер пачки для обработки</param>
+    /// <param name="cancellation"</param>
+    /// <returns></returns>
+    IAsyncEnumerable<IEnumerable<Booking>> GetPendingBookingsAsync(int chunkSize, CancellationToken cancellation);
 }
--- EventManager/Data/BookingRepository.cs
+++ EventManager/Data/BookingRepository.cs
@@ -1,9 +1,13 @@
+using System.Runtime.CompilerServices;
 using EventManager.Application.Interfaces;
 using EventManager.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventManager.Data;
-
+/// <summary>
+/// Реализация интерфейса репозитория броней.
+/// </summary>
+/// <param name="dbContext"></param>
 public class BookingRepository(AppDbContext dbContext) : IBookingRepository
 {
     async Task IBookingRepository.AddBooking(Booking booking, CancellationToken cancellation)
@@ -24,5 +28,33 @@ public class BookingRepository(AppDbContext dbContext) : IBookingRepository
             dbContext.Events.Update(booking.Event);
         }
         await dbContext.SaveChangesAsync(cancellation);
+    }
+
+    async IAsyncEnumerable<IEnumerable<Booking>> IBookingRepository.GetPendingBookingsAsync(int chunkSize, [EnumeratorCancellation] CancellationToken cancellation)
+    {
+        if (chunkSize <= 0)
+            throw new ArgumentException("Chunk size must be greater than 0", nameof(chunkSize));
+
+        var pendingBookings = dbContext.Bookings
+            .AsNoTracking()
+            .Where(b => b.Status == BookingStatus.Pending)
+            .Include(b => b.Event)
+            .OrderBy(b => b.CreatedAt)
+            .AsAsyncEnumerable()
+            .WithCancellation(cancellation);
+
+        var chunk = new List<Booking>(chunkSize);
+        await foreach(var item in pendingBookings)
+        {
+            chunk.Add(item);
+            if (chunk.Count == chunkSize)
+            {
+                yield return chunk;
+                chunk = new(chunkSize);
+            }
+        }
+
+        if (chunk.Count >0 )
+            yield return chunk;
     }
 }
